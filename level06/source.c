a9f883ee458abddb85f99527d7a0a7bb3cdb35ef
Jean-Baptiste Terrazzoni <jterrazz@icloud.com>
2020-02-24 13:30:47 +0100

    Noice

--- level06/source.c
+++ level06/source.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ptrace.h>
+
+int auth(login_str ebp + 0x8, Serial ebp + 0xc)
+{
+	int	login_len; // EBP - 0xc
+
+	login_str[strcspn('\n', login_str)] = 0;
+	login_len = strnlen(login_str, 32);
+	
+	if (login_len != 5) {
+		return 1;
+	}
+
+	if (ptrace(0, 0, 1, 0) == 0xffffffff) { // ???
+		puts("\e[32m.---------------------------.");
+		puts("\e[31m| !! TAMPERING DETECTED !!  |");
+		puts("\e[32m.---------------------------.");
+		return 1;
+	}
+	
+	int value; // EBP - 0x10
+	value = (int) (login_str[3]) ^ 0x1337 + 0x5eeded;
+
+}
 
 int main()
 {
@@ -7,14 +31,6 @@ int main()
 
 	scanf(%u, esp + 0x28);	Serial
 
-	if (auth(Login, Serial))
+	if (!auth(Login, Serial))
 		system("/bin/sh");
-}
-
-auth(Login ebp + 0x8, Serial ebp + 0xc)
-{
-	int	temp;
-	Login[strcspn('\n', Login)] = '\0';	Replaces '\n' with '\0'
-	temp = strnlen(Login, 32);
-	
 }
